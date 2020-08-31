73b2fb27aca18a6872dd7f41e8f55372967c6055
duchn.dev <duchn.dev@gmail.com>
2020-08-31 13:37:56 +0700

    --amend

--- inc/class-sc-widgets.php
+++ inc/class-sc-widgets.php
@@ -16,27 +16,31 @@ if ( ! class_exists( 'SC_Widgets' ) ) :
 
 		    if ( is_array( $sidebars_widgets ) ) {
 		        foreach ( $sidebars_widgets as $key => $value ) {
-
-		            if(is_array($value)){
-		                foreach ($value as $widget_id) {
-		                    $pieces = explode('-', $widget_id);
-		                    $multi_number = array_pop($pieces);
+		        	$widget_added = [];
+		            if( is_array( $value ) ) {
+		                foreach ( $value as $widget_id ) {
+		                    $pieces = explode( '-', $widget_id );
+		                    $multi_number = array_pop( $pieces );
 		                    $id_base = implode('-', $pieces);
 		                    $widget_data = get_option( 'widget_' . $id_base );
 
 		                    // Remove inactive widgets
-		                    if( $key != 'wp_inactive_widgets' ) {
+		                    if( $key !== 'wp_inactive_widgets' && ! in_array( $id_base, $widget_added ) ) {
+		                    	$widget_added[] = $id_base;
 		                    	if ( ! isset( $result[$key] ) ) {
 		                    		$result[$key] = array();
 		                    	}
 		                        unset( $widget_data['_multiwidget'] );
-		                        $result[$key][$id_base] = array( $id_base, $widget_data );
+		                        foreach ( $widget_data as $widget ) {
+		                        	if ( $widget ) {
+		                        		$result[$key][] = array( $id_base, $widget );
+		                        	}
+		                        }
 		                    }
 		                }
 		            }
 		        }
 		    }
-
 		    if ( $sidebar_id ) {
 		        return isset( $result[$sidebar_id] ) ? $result[ $sidebar_id ] : [];
 		    }
